ed7dbe19d353de2720300669ac6dc18745fe8e19
root <root@moredevs7.(none)>
2012-04-20 13:43:21 +0300

    Added mkdir.php to create tmp directory structure

---
+++ mkdir.php
@@ -0,0 +1,31 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+echo '<pre>';
+
+chdir(sys_get_temp_dir());
+mkdir('flutter-club');
+chdir('flutter-club');
+
+$dirs = explode(' ', 'backup cache log package scaffold session package/archives package/manifests package/packages package/repositories package/sdk');
+foreach ($dirs as $dir) {
+	mkdir($dir);
+	file_put_contents($dir.'/index.html', "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
+<html>
+  <head>
+    <title></title>
+    <meta http-equiv=\"refresh\" content=\"0;url=/\">
+  </head>
+  <body>
+  </body>
+</html>
+");
+}
+
+file_put_contents('.session', "allow from none
+deny from all
+");
+
+echo '<hr />DONE';
+
