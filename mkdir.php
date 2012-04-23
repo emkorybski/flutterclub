d5c89094d4f6eb61b520d24f3253eb022c46c576
Florin <f@f>
2012-04-23 12:37:34 +0300

    test

--- mkdir.php
+++ mkdir.php
@@ -4,17 +4,16 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 echo '<pre>';
 
-passthru('ls ' . sys_get_temp_dir());
-
-/*
-
 chdir(sys_get_temp_dir());
 mkdir('flutter-club');
 chdir('flutter-club');
 
 $dirs = explode(' ', 'backup cache log package scaffold session package/archives package/manifests package/packages package/repositories package/sdk');
 foreach ($dirs as $dir) {
+	echo '<hr />';
 	mkdir($dir);
+	chmod($dir, 0777);
+	print_r(stat($dir));
 	file_put_contents($dir.'/index.html', "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
 <html>
   <head>
@@ -38,5 +37,7 @@ foreach (glob('*') as $dir) {
     echo $dir.' ; ';
 }
 
-*/
+echo '<br />';
+
+passthru('ls -al ' . sys_get_temp_dir());
 
