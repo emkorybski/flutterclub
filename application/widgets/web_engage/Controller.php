52f9dbe7346a9ea45c8a97561c9b303c411107f3
Paul Negrutiu <paul.negrutiu@gmail.com>
2012-07-18 14:34:40 +0300

    WebEngage

---
+++ application/widgets/web_engage/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+class Widget_Web_EngageController extends Engine_Content_Widget_Abstract
+{
+	public function indexAction()
+	{
+	}
+}
---
+++ application/widgets/web_engage/manifest.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+	'package' => array(
+		'type' => 'widget',
+		'name' => 'web_engage',
+		'version' => '4.0.0',
+		'revision' => '$Revision: 8292 $',
+		'path' => 'application/widgets/web_engage',
+		'repository' => 'socialengine.net',
+		'title' => 'WebEngage',
+		'description' => 'WebEngage',
+		'author' => 'me',
+		'directories' => array(
+			'application/widgets/web_engage',
+		),
+	),
+	// Backwards compatibility
+	'type' => 'widget',
+	'name' => 'web_engage',
+	'version' => '4.0.0',
+	'revision' => '$Revision: 8292 $',
+	'title' => 'WebEngage',
+	'description' => 'WebEngage',
+	'author' => 'me',
+	'category' => 'Widgets',
+);
