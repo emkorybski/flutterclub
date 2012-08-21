94b6a8b25e708bfe3e7b1f52a3e92055e11288e1
Paul Negrutiu <paul.negrutiu@gmail.com>
2012-08-22 00:31:49 +0300

    competition leaderboard for friends widget

---
+++ application/widgets/fc_competition_leaderboard_friends/Controller.php
@@ -0,0 +1,50 @@
+﻿<?php
+require_once('custom/config.php');
+require_once(PATH_LIB . 'fc.php');
+require_once(PATH_DOMAIN . 'user.php');
+require_once(PATH_DOMAIN . 'user_balance.php');
+
+class Widget_FC_Competition_Leaderboard_FriendsController extends Engine_Content_Widget_Abstract
+{
+	public function indexAction()
+	{
+		$competition = \bets\Competition::getCurrent();
+
+		bets\bets::sql()->multiQuery("call fc_sp_get_competition_leaderboard($competition->id)");
+		$leaderboardData = bets\bets::sql()->getResult();
+		while (bets\bets::sql()->moreResults()) {
+			bets\bets::sql()->getResult();
+			bets\bets::sql()->nextResult();
+		}
+
+		$seCurrentUser = Engine_Api::_()->user()->getViewer();
+		$friends = Zend_Paginator::factory($seCurrentUser->membership()->getMembersOfSelect());
+
+		$userFriendsIds = array($seCurrentUser->user_id);
+		foreach ($friends as $friend) {
+			$userFriendsIds[] = $friend->resource_id;
+		}
+
+		$leaderboard = array();
+		$position = 1;
+		foreach ($leaderboardData as $leaderboardUserData) {
+			$fcUser = \bets\User::get($leaderboardUserData['iduser']);
+			$seUser = Engine_Api::_()->user()->getUser($fcUser->id_engine4_users);
+
+			if (!in_array($seUser->user_id, $userFriendsIds))
+				continue;
+
+			$successRate = \bets\fc::percentage($leaderboardUserData['won_count'], $leaderboardUserData['bet_count']);
+			$userData = array(
+				'position' => $position,
+				'user' => $seUser,
+				'profit' => number_format($leaderboardUserData['profit'], 2, '.', ' '),
+				'won_count' => $leaderboardUserData['won_count'],
+				'bet_count' => $leaderboardUserData['bet_count'],
+				'successRate' => $successRate);
+			$leaderboard[] = $userData;
+			$position++;
+		}
+		$this->view->leaderboardUsers = $leaderboard;
+	}
+}
---
+++ application/widgets/fc_competition_leaderboard_friends/manifest.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+return array(
+	'package' => array(
+		'type' => 'widget',
+		'name' => 'fc_competition_leaderboard_friends',
+		'version' => '4.0.0',
+		'revision' => '$Revision: 8292 $',
+		'path' => 'application/widgets/fc_competition_leaderboard_friends',
+		'repository' => 'socialengine.net',
+		'title' => 'FC Leaderboard Friends',
+		'description' => 'FC Leaderboard Friends',
+		'author' => 'me',
+		'directories' => array(
+			'application/widgets/fc_competition_leaderboard_friends',
+		),
+	),
+	// Backwards compatibility
+	'type' => 'widget',
+	'name' => 'fc_competition_leaderboard_friends',
+	'version' => '4.0.0',
+	'revision' => '$Revision: 8292 $',
+	'title' => 'FC Leaderboard Friends',
+	'description' => 'FC Leaderboard Friends',
+	'author' => 'me',
+	'category' => 'Widgets',
+);
