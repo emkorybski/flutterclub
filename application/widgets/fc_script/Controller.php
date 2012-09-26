4f51109f878ac13ae8e880bbf323b600e1027a0d
Paul Negrutiu <paul.negrutiu@gmail.com>
2012-09-26 17:53:49 +0300

    competition settlement script (not finished)

--- application/widgets/fc_script/Controller.php
+++ application/widgets/fc_script/Controller.php
@@ -5,7 +5,6 @@ require_once(PATH_LIB . 'bet_validator.php');
 
 class Widget_FC_ScriptController extends Engine_Content_Widget_Abstract
 {
-
 	public function indexAction()
 	{
 		if (!empty($_REQUEST['action'])) {
@@ -21,11 +20,112 @@ class Widget_FC_ScriptController extends Engine_Content_Widget_Abstract
 		$betValidator->validateBets();
 	}
 
-	public function sendEmailAction()
+	public function competitionSettlementAction()
 	{
-		$userData = \bets\User::getCurrentUserData(13);
-		\Engine_Api::_()->getApi('mail', 'core')->sendSystem($userData['email'], 'notify_bet_settlement', array(
-			'bet_data' => '[bet data here]'
-		));
+		$currentDate = new \DateTime();
+		$settlementDate = $currentDate->sub(new \DateInterval("P1D"));
+
+		$competition = \bets\Competition::getWhere(array('settled=' => 'n', 'ts_end<' => $settlementDate->format('Y-m-d H:i:s')));
+		if (!$competition)
+			return;
+
+		$this->competition = $competition;
+
+		$this->voidPendingBets();
+
+		$leaderboard = $this->getCompetitionLeaderboard();
+		$emailTextBody = "Position, Name, Profit, Success Rate" . "\r\n";
+		$emailHtmlBody = "<table border='1' bordercolor='#000000' cellpadding='5' cellspacing='0'>";
+		$emailHtmlBody .= "
+			<tr>
+				<th>Position</th>
+				<th>Name</th>
+				<th>Profit</th>
+				<th>Success Rate</th>
+			</tr>";
+		foreach ($leaderboard as $data) {
+			$emailHtmlBody .= "<tr>";
+			$emailHtmlBody .= "<td>" . $data['position'] . "</td>";
+			$emailHtmlBody .= "<td>" . $data['user'] . "</td>";
+			$emailHtmlBody .= "<td>" . number_format($data['profit'], 2, '.', ',') . "</td>";
+			$emailHtmlBody .= "<td>" . $data['successRate'] . " ";
+			$emailHtmlBody .= "(" . $data['won_count'] . "/" . $data['bet_count'] . ")</td>";
+			$emailHtmlBody .= "</tr>";
+
+			$emailTextBody .= $data['position'] . ", ";
+			$emailTextBody .= $data['user'] . ", ";
+			$emailTextBody .= number_format($data['profit'], 2, '.', ',') . ", ";
+			$emailTextBody .= $data['successRate'] . " ";
+			$emailTextBody .= "(" . $data['won_count'] . "/" . $data['bet_count'] . ")";
+			$emailTextBody .= "\r\n";
+		}
+		$emailHtmlBody .= "</table>";
+
+		try {
+			$fromAddress = Engine_Api::_()->getApi('settings', 'core')->getSetting('core.mail.from', 'admin@' . $_SERVER['HTTP_HOST']);
+			$fromName = Engine_Api::_()->getApi('settings', 'core')->getSetting('core.mail.name', 'Site Admin');
+
+			$recipientEmail = 'paul.negrutiu@gmail.com';
+			$recipientName = 'Paul Negrutiu';
+			$subject = "Competition settlement - Leaderboard.";
+
+			$mailApi = Engine_Api::_()->getApi('mail', 'core');
+			$mail = $mailApi->create();
+
+			$mail->addTo($recipientEmail, $recipientName);
+			$mail->setFrom($fromAddress, $fromName);
+			$mail->setSubject($subject);
+			//$mail->setBodyHtml($emailHtmlBody);
+			$mail->setBodyText($emailTextBody);
+			$res = $mailApi->sendRaw($mail);
+		} catch (Exception $e) {
+		}
+	}
+
+	private function voidPendingBets()
+	{
+		$pendingBets = \bets\Bet::findWhere(array('idcompetition=' => $this->competition->id, 'status=' => 'pending'));
+		foreach ($pendingBets as $pendingBet) {
+			$pendingSelections = \bets\BetSelection::findWhere(array('idbet=' => $pendingBet->id, 'status=' => 'pending'));
+			foreach ($pendingSelections as $pendingSelection) {
+				$pendingSelection->odds = 1;
+				$pendingSelection->status = 'void';
+				$pendingSelection->update();
+			}
+		}
+
+		$betValidator = new \bets\BetValidator();
+		$betValidator->validateBets();
+	}
+
+	private function getCompetitionLeaderboard()
+	{
+		$idCompetition = $this->competition->id;
+		\bets\bets::sql()->multiQuery("call fc_sp_get_competition_leaderboard($idCompetition)");
+		$leaderboardData = \bets\bets::sql()->getResult();
+		while (\bets\bets::sql()->moreResults()) {
+			\bets\bets::sql()->getResult();
+			\bets\bets::sql()->nextResult();
+		}
+
+		$leaderboard = array();
+		$position = 1;
+		foreach ($leaderboardData as $leaderboardUserData) {
+			$fcUser = \bets\User::get($leaderboardUserData['iduser']);
+			$seUser = Engine_Api::_()->user()->getUser($fcUser->id_engine4_users);
+			$successRate = \bets\fc::getPercentage($leaderboardUserData['won_count'], $leaderboardUserData['bet_count']);
+
+			$userData = array(
+				'position' => $position,
+				'user' => $seUser->getTitle(),
+				'profit' => number_format($leaderboardUserData['profit'], 2, '.', ','),
+				'won_count' => $leaderboardUserData['won_count'],
+				'bet_count' => $leaderboardUserData['bet_count'],
+				'successRate' => $successRate);
+			$leaderboard[] = $userData;
+			$position++;
+		}
+
+		return $leaderboard;
 	}
 }
