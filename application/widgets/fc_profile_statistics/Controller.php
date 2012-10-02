4356805b44daa647c6895b17fb5fc3a1ef580b0d
Paul Negrutiu <paul.negrutiu@gmail.com>
2012-10-02 18:51:08 +0300

    user statistics per current competition

--- application/widgets/fc_profile_statistics/Controller.php
+++ application/widgets/fc_profile_statistics/Controller.php
@@ -8,19 +8,49 @@ class Widget_FC_Profile_StatisticsController extends Engine_Content_Widget_Abstr
 {
 	public function indexAction()
 	{
-		// Test values
-		$ranking = 10;
-		$num_bets = 10;
-		$num_winning_bets = 10;
-		$num_losing_bets = 10;
-		$success_rate = 50;
-		$profit = 100;
+		if (!Engine_Api::_()->core()->hasSubject()) {
+			$this->setNoRender();
+			return;
+		}
 
-		$this->view->ranking = $ranking;
-		$this->view->num_bets = $num_bets;
-		$this->view->num_winning_bets = $num_winning_bets;
-		$this->view->num_losing_bets = $num_losing_bets;
-		$this->view->success_rate = $success_rate;
-		$this->view->profit = $profit;
+		$subject = Engine_Api::_()->core()->getSubject();
+
+		$competition = \bets\Competition::getCurrent();
+		$user = \bets\User::getUser($subject);
+
+		bets\bets::sql()->multiQuery("call fc_sp_get_user_stats($competition->id, $user->id)");
+		$userStats = bets\bets::sql()->getResult();
+		while (bets\bets::sql()->moreResults()) {
+			bets\bets::sql()->getResult();
+			bets\bets::sql()->nextResult();
+		}
+
+		if (!isset($userStats[0])) {
+			$this->view->ranking = '-';
+			$this->view->num_bets = 0;
+			$this->view->num_winning_bets = 0;
+			$this->view->num_losing_bets = 0;
+			$this->view->success_rate = '-';
+			$this->view->profit = \bets\fc::formatDecimalNumber(0);
+			return;
+		}
+
+		$userStats = $userStats[0];
+		$userProfit = $userStats['profit'];
+
+		bets\bets::sql()->multiQuery("call fc_sp_get_user_position($competition->id, $userProfit)");
+		$userPosition = bets\bets::sql()->getResult();
+		while (bets\bets::sql()->moreResults()) {
+			bets\bets::sql()->getResult();
+			bets\bets::sql()->nextResult();
+		}
+		$userPosition = $userPosition[0];
+
+		$this->view->ranking = $userPosition['pos'];
+		$this->view->num_bets = $userStats['bet_count'];
+		$this->view->num_winning_bets = $userStats['won_count'];
+		$this->view->num_losing_bets = $userStats['lost_count'];
+		$this->view->success_rate = \bets\fc::getPercentage($userStats['won_count'], $userStats['bet_count']);
+		$this->view->profit = \bets\fc::formatDecimalNumber($userProfit);
 	}
 }
